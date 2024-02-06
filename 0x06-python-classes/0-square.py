c3e19a60a0850cd273cf87fa06d3196bbf5ce3cd
AA-FARUQ <aretheywhome@gmail.com>
2024-02-06 10:55:25 -0800

    My commit message for 0x06 Project

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""  # Updated comment
+
+class Square:  # Updated class name
+    """Represent a square."""  # Updated comment
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This is my Readme for 0x06 Project
